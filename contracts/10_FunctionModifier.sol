55e92be8a05e753e8b5881bc12d60343bc4e1933
JamesVictor-O <victorjames40v@gmail.com>
2025-01-01 02:18:16 +0100

    learnt about modifires

---
+++ contracts/10_FunctionModifier.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract FunctionModifier{
+    address public owner;
+    uint public x=10;
+    bool public locked;
+
+    constructor(){
+        owner=msg.sender;
+    }
+    
+    modifier onlyOwner(){
+        require(msg.sender== owner,"Not owner");
+        _;
+    }
+    modifier  validAddress(address _addr){
+        require(_addr != address(0), "Not valid address");
+        _;
+    }
+
+    // function changeOwner(address)
+
+}
