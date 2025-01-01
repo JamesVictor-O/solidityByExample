dcfbb36ffde2547fff900cb9e8f3a1c6d45807f4
JamesVictor-O <victorjames40v@gmail.com>
2025-01-01 12:56:50 +0100

    learnt about modifiers and how to prevent reentrancy attack

--- contracts/10_FunctionModifier.sol
+++ contracts/10_FunctionModifier.sol
@@ -10,15 +10,32 @@ contract FunctionModifier{
         owner=msg.sender;
     }
     
-    modifier onlyOwner(){
-        require(msg.sender== owner,"Not owner");
-        _;
-    }
-    modifier  validAddress(address _addr){
-        require(_addr != address(0), "Not valid address");
-        _;
-    }
+   modifier  onlyOwner(){
+     require(msg.sender == owner,"Not owner");
+     _;
+   }
 
-    // function changeOwner(address)
+   modifier  validateAddress(address _address){
+    require(_address != address(0), "Address Not valid");
+    _;
+   }
+   
+   function changeOwner(address _newOwner) public  onlyOwner validateAddress(_newOwner){
+       owner=_newOwner;
+   }
+   
+   modifier  noReentrancy(){
+      require(!locked, "No reentrancy");
+      locked=true;
+      _;
 
+      locked= false;
+   }
+
+   function decrement(uint256 i) public noReentrancy{
+     x-=i;
+     if(i > 1){
+        decrement(i-1);
+     }
+   }
 }
